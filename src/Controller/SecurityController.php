1f8c015ee6a660045356da4410df1ea676e2e8a5
delphine.millot <delfymillot@gmail.com>
2018-06-16 16:30:44 +0200

    ajout des repos du tableau de bord

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 
 namespace App\Controller;
 
+use App\Entity\Observation;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -13,21 +14,24 @@ use Twig\Environment;
 class SecurityController extends Controller
 {
 
-	/**
-	 * @Route("/signin", name="signin")
-	 */
-	public function signin(Request $request, AuthenticationUtils $authenticationUtils)
-	{
-         $error = $authenticationUtils->getLastAuthenticationError();
-         $lastUsername = $authenticationUtils->getLastUsername();
+    /**
+     * @Route("/signin", name="signin")
+     * @param Request $request
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function signin(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-         return $this->render('signin.html.twig', array(
-         		'last_username' => $lastUsername,
-         		'error'			=> $error,
-         ));
-         }
+        return $this->render('signin.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'			=> $error,
+        ));
+    }
 
-     /**
+    /**
      * @Route("/adminSpace")
      * @param Environment $twig
      * @return Response
@@ -35,12 +39,25 @@ class SecurityController extends Controller
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-        public function adminSpace(Environment $twig){
-            $username = $this->getUser()->getUsername();
-            return new Response($twig->render('pages/adminSpace.html.twig',[
-                'username' => $username
-                ]));
-        }
+    public function adminSpace(Environment $twig){
+        $username = $this->getUser()->getUsername();
+
+        $obs = $this->getDoctrine()
+            ->getRepository(Observation::class);
+
+        $observations = $obs->findByUserId(1); //id à envoyer dynamiquement
+
+        $validatesObs = $obs->findByStatus(1); //id à envoyer dynamiquement
+
+        //  var_dump($validatesObs);
+
+        return new Response($twig->render('pages/adminSpace.html.twig',[
+            'username' => $username,
+            'observations' => $observations,
+            'validatesObs' => $validatesObs
+        ]));
+    }
+
 }
 
 
