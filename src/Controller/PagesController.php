d1f01154bb8e09513a450a6c8e5614bb91a4538d
delphine.millot <delfymillot@gmail.com>
2018-06-16 15:10:40 +0200

    Gestion des conflits

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * Created by PhpStorm.
@@ -24,7 +23,8 @@ class PagesController
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function index(Environment $twig){
+    public function index(Environment $twig)
+    {
         return new Response($twig->render('pages/index.html.twig'));
     }
 
@@ -36,7 +36,8 @@ class PagesController
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function home(Environment $twig){
+    public function home(Environment $twig)
+    {
         return new Response($twig->render('pages/index.html.twig'));
     }
 
@@ -48,7 +49,8 @@ class PagesController
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function test(Environment $twig){
+    public function test(Environment $twig)
+    {
         // return new Response('test');
         return new Response($twig->render('pages/index.html.twig'));
     }
@@ -61,7 +63,8 @@ class PagesController
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function observForm(Environment $twig){
+    public function observForm(Environment $twig)
+    {
         return new Response($twig->render('pages/observForm.html.twig'));
     }
 
@@ -73,7 +76,8 @@ class PagesController
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function about(Environment $twig){
+    public function about(Environment $twig)
+    {
         return new Response($twig->render('pages/about.html.twig'));
     }
 
@@ -86,7 +90,8 @@ class PagesController
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function contact(Environment $twig){
+    public function contact(Environment $twig)
+    {
         return new Response($twig->render('pages/contact.html.twig'));
     }
 
@@ -98,53 +103,8 @@ class PagesController
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function adminSpace(Environment $twig){
+    public function adminSpace(Environment $twig)
+    {
         return new Response($twig->render('pages/adminSpace.html.twig'));
     }
-=======
-<?php
-/**
- * Created by PhpStorm.
- * User: delphinemillotpedrero
- * Date: 11/05/2018
- * Time: 23:21
- */
-
-namespace App\Controller;
-
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-class PagesController
-{
-    /**
-     * @Route("/")
-     * @param Environment $twig
-     * @return Response
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
-     
-    public function index(Environment $twig){
-        return new Response($twig->render('pages/index.html.twig'));
-    }*/
-
-    /**
-     * @Route("/home", name="home")
-     * @param Environment $twig
-     * @return Response
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
-     */
-    public function home(Environment $twig){
-        return new Response($twig->render('pages/index.html.twig'));
-    }
-
-
-
-
->>>>>>> 15bd4a56fa07dcf3c75bed10e4453a6d5bca8332
 }
