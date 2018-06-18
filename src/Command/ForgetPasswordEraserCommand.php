27025986d481bf69045fcbbaebc36911afea93dc
Fatboy42 <palmino.angelo@gmail.com>
2018-06-18 13:37:46 +0200

    Ajout CRON

--- src/Command/ForgetPasswordEraserCommand.php
+++ src/Command/ForgetPasswordEraserCommand.php
@@ -5,9 +5,18 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ForgetPasswordEraserCommand extends Command
 {
+	private $em;
+
+	public function __construct(EntityManagerInterface $em)
+	{
+		parent::__construct();
+		$this->em = $em;
+	}
+
 	protected function configure()
 	{
 		$this
@@ -19,5 +28,21 @@ class ForgetPasswordEraserCommand extends Command
 	protected function execute(InputInterface $inputinterface, OutputInterface $outputinterface)
 	{
 		$outputinterface->writeLn('Test');
+
+		$oldForgetPassword = $this->em->getRepository('App:ForgotPassword')->createQueryBuilder('p')
+			->andWhere("p.request_date < DATE_ADD(CURRENT_DATE(),'-5', 'hour')")
+			->getQuery()
+			->getResult(); //Ecrire directement en DQL ?
+
+		if (false === empty($oldForgetPassword)) 
+		{
+			foreach ($oldForgetPassword as $key) 
+			{
+				$this->em->remove($key);
+			}	
+		  $this->em->flush();	
+		}
+
+		//var_dump($oldForgetPassword);
 	}
 }
