ec473f155ce2ffc1c5a4061b5a41932b0c50ffb4
delphine.millot <delfymillot@gmail.com>
2018-06-24 00:04:52 +0200

    préparation du template d'affichage d'obs et des fonctions js correspondantes

--- src/Form/ObsDisplayType.php
+++ src/Form/ObsDisplayType.php
@@ -10,7 +10,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class ObservationType extends AbstractType
+class ObsDisplayType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
